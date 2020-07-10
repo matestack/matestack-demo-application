a3d7ec6c82fe9ce838c2410afc521b89508a9278
Pascal Wengerter <pascal@wengerter.info>
2020-07-10 13:02:33 +0200

    Refactor person index page to use collection component

--- app/matestack/pages/demo_app/persons/index.rb
+++ app/matestack/pages/demo_app/persons/index.rb
@@ -1,21 +1,63 @@
 class Pages::DemoApp::Persons::Index < Matestack::Ui::Page
+  include Matestack::Ui::Core::Collection::Helper
 
-	def prepare
-		@persons = Person.all
-	end
+  def prepare
+    person_collection_id = "person-collection"
 
-	def response
-		components {
-			ul do
-				@persons.each do |person|
-					li do
-						plain "#{person.first_name} #{person.last_name} "
-						transition path: :person_path, params: {id: person.id}, text: '(Details)'
-					end
-				end
-			end
-			transition path: :new_person_path, text: 'Create new person'
-		}
-	end
+    current_filter = get_collection_filter(person_collection_id)
+
+    person_query = Person.all
+
+    filtered_person_query = person_query
+    .where("last_name LIKE ?", "%#{current_filter[:last_name]}%")
+
+    @person_collection = set_collection({
+      id: person_collection_id,
+      data: filtered_person_query
+    })
+  end
+
+  def response
+    components {
+      partial :filter
+
+      async rerender_on: "person-collection-update" do
+        partial :content
+      end
+
+      transition path: :new_person_path, text: 'Create new person'
+    }
+  end
+
+  def filter
+    partial {
+      collection_filter @person_collection.config do
+        collection_filter_input key: :last_name, type: :text, placeholder: "Filter by Last name"
+        collection_filter_submit do
+          button text: "Apply"
+        end
+        collection_filter_reset do
+          button text: "Reset"
+        end
+      end
+    }
+  end
+
+  def content
+    partial {
+      collection_content @person_collection.config do
+
+        ul do
+          @person_collection.data.each do |person|
+            li do
+              plain "#{person.first_name} #{person.last_name} "
+              transition path: :person_path, params: {id: person.id}, text: '(Details)'
+            end
+          end
+        end
+
+      end
+    }
+  end
 
 end
