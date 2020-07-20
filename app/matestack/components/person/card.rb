8a1e822246459b09f075496a9e6c00bae49112d0
Pascal Wengerter <pascal@wengerter.info>
2020-07-20 15:55:10 +0200

    update custom components with (custom) styling

--- app/matestack/components/person/card.rb
+++ app/matestack/components/person/card.rb
@@ -6,9 +6,11 @@ class Components::Person::Card < Matestack::Ui::StaticComponent
 
   def response
     components {
-      div do
-        paragraph text: "#{@person.first_name} #{@person.last_name} "
-        transition path: :person_path, params: {id: @person.id}, text: '(Details)'
+      div class: 'card' do
+        div class: 'card-body' do
+          paragraph text: "#{@person.first_name} #{@person.last_name}"
+          transition path: :person_path, params: {id: @person.id}, class: 'btn btn-primary', text: 'Details'
+        end
       end
     }
   end
--- app/matestack/components/person/disclaimer.rb
+++ app/matestack/components/person/disclaimer.rb
@@ -2,9 +2,9 @@ class Components::Person::Disclaimer < Matestack::Ui::DynamicComponent
 
   def response
     components {
-      div attributes: {"v-show": "show == true"} do
-        paragraph text: 'None of the presented names belong to and/or are meant to refer to existing human beings. They were created using a "Random Name Generator".'
-        button attributes: {"@click": "show = false"}, text: 'Hide'
+      div id: 'disclaimer', class: 'container-fluid text-center', attributes: {"v-show": "show == true"} do
+        span text: 'None of the presented names belong to and/or are meant to refer to existing human beings. They were created using a "Random Name Generator".'
+        button class: 'btn btn-warning', attributes: {"@click": "show = false"}, text: 'Hide'
       end
     }
   end
