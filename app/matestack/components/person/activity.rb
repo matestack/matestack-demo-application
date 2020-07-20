81a06fac1a50b64cdeefddfa625d898417966533
Pascal Wengerter <pascal@wengerter.info>
2020-07-20 16:44:01 +0200

    Style forgotten custom component

--- app/matestack/components/person/activity.rb
+++ app/matestack/components/person/activity.rb
@@ -2,15 +2,21 @@ class Components::Person::Activity < Matestack::Ui::DynamicComponent
 
   def response
     components {
-      div do
-        paragraph do
-          plain 'Need ideas on what to do with one of these persons?'
-          button attributes: {"@click": "addActivity()"}, text: 'Click here'
-        end
-        ul attributes: {"v-if": "activities.length"} do
-          li attributes: {"v-for": "activity,index in activities"} do
-            plain "{{activity}}"
-            button attributes: {"@click": "deleteActivity(index)"}, text: 'Remove'
+      section do
+        div class: 'container' do
+          div class: 'row' do
+            div class: 'col-md-6 offset-md-3' do
+              paragraph class: 'text-center' do
+                plain 'Need ideas on what to do with one of these persons?'
+                button attributes: {"@click": "addActivity()"}, class: 'btn btn-primary', text: 'Click here'
+              end
+              ul attributes: {"v-if": "activities.length"}, class: 'list-group' do
+                li attributes: {"v-for": "activity,index in activities"}, class: 'list-group-item d-flex justify-content-between align-items-center' do
+                  plain "{{activity}}"
+                  span attributes: {"@click": "deleteActivity(index)"}, class: 'badge badge-primary badge-pill', text: 'X'
+                end
+              end
+            end
           end
         end
       end
